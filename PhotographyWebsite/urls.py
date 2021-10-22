512bee042858200a8b438abe3d5be60e4e2618cd
Azubuike <X00191169@myTUDublin.ie>
2021-10-22 22:49:59 +0100

    Completed Image upload functionality

--- PhotographyWebsite/urls.py
+++ PhotographyWebsite/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('', include('photos.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
