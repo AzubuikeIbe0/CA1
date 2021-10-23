98e122d45190a4dcc5d59babbc0ebe6a5ca20cee
Azubuike <X00191169@myTUDublin.ie>
2021-10-23 13:38:02 +0100

    Added Photo title, Author & Date

--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.urls import reverse
+from django.db.models import DateTimeField
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
@@ -9,9 +14,20 @@ class Category(models.Model):
 
 
 class Photo(models.Model):
+    title = models.CharField(max_length=30, default="Photo Title")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(null=False, blank=False)
     description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        default = None,
+        on_delete=models.CASCADE,
+    )
+
 
     def __str__(self):
         return self.description
+
+    def get_absolute_url(self):
+        return reverse('photo', args=[str(self.id)])
